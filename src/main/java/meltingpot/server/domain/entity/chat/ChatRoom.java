8b653a34b157485f3b1e3dd85d32897be2dfab8f
moonyaeyoon <moonyaeyoon@moonyaeyoon.com>
2024-05-22 20:06:35 +0900

    feat: Swagger 설정

--- src/main/java/meltingpot/server/domain/entity/chat/ChatRoom.java
+++ src/main/java/meltingpot/server/domain/entity/chat/ChatRoom.java
@@ -2,6 +2,7 @@ package meltingpot.server.domain.entity.chat;
 
 import jakarta.persistence.*;
 import lombok.*;
+import meltingpot.server.domain.entity.Party;
 import meltingpot.server.domain.entity.common.BaseEntity;
 
 import java.util.ArrayList;
